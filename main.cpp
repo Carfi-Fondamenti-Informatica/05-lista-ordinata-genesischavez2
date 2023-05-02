81040b9b18ddbe218054dc239a92d650564309cc
genesischavez2 <127029593+genesischavez2@users.noreply.github.com>
2023-05-02 16:15:54 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -2,5 +2,26 @@
 using namespace std;
 
 int main() {
+   
+ int lista[10];
+ for(int i=0;i<10;i++){
+ cin >> lista[i];
+ }
+ int a=0;
+ cin >> a;
+ 
+ int variabile=0;
+ for(int i=0;i<10;i++){
+    if(a<=lista[i]){
+       variabile=lista[i];
+       lista[i]=a;
+       a=variabile;
+    }
+ }
+       
+ for(int i=0;i<10;i++){
+ cout << lista[i] << " ";
+ }
+ 
    return 0;
 }
